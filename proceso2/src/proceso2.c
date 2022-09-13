334344c9e5a4dd1e7af36a83efb2488b41146e27
camilamarinio <camarino@frba.utn.edu.ar>
2022-09-13 03:13:48 +0000

    Configuraciones generales

---
+++ proceso2/src/proceso2.c
@@ -0,0 +1,13 @@
+#include "proceso2.h"
+
+int main(int argc, char ** argv){
+    if(argc > 1 && strcmp(argv[1],"-test")==0)
+        return run_tests();
+    else{  
+        t_log* logger = log_create("./proceso1.log", "PROCESO1", true, LOG_LEVEL_INFO);
+        printf("esta ok");
+        log_info(logger, "Soy el proceso 1! %s", mi_funcion_compartida());
+        log_destroy(logger);
+    } 
+
+}
